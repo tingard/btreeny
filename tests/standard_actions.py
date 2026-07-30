d97165c7fe14d5f33c44f6dcc81a88e00feac499
Tim Lingard <tklingard@gmail.com>
2026-07-30 07:20:15 +0100

    remove prints in tests

--- tests/standard_actions.py
+++ tests/standard_actions.py
@@ -27,10 +27,8 @@ def never_runs(b: Any):
 
 @bt.action
 def run_then(node_id: IdType, result: bt.TreeStatus = bt.SUCCESS, count: int = 1):
-    print(f"Setting up node {node_id}")
     c = itertools.chain([bt.RUNNING] * count, itertools.repeat(result))
     yield lambda b: next(c)
-    print(f"Tearing down node {node_id}")
 
 
 run_then_ok = functools.partial(run_then, result=bt.SUCCESS)
